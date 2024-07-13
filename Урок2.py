08234f05c1973d898ebae4874cacecde05723735
AliaMAK <altana1978@mail.ru>
2024-07-14 00:07:43 +0800

    Add files via upload

---
+++ Урок2.py
@@ -0,0 +1,5 @@
+homework_count = 12
+hours_used_count = 1.5
+course_name = "Python"
+print("Курс",course_name,"всего задач:",homework_count,"затрачено часов:",hours_used_count,)
+print("среднее время выполнения",hours_used_count / homework_count,"часов")
